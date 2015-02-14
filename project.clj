0d2c9c39502be82ae67701f96205393bb0d280fb
Juho Teperi <juho.teperi@iki.fi>
2015-02-14 23:00:33 +0200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/om-dev-tools "0.1.0-SNAPSHOT"
+(defproject metosin/om-dev-tools "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
