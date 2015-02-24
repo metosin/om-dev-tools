03417ed5755e5dd887e9613f7725db161c78751a
Juho Teperi <juho.teperi@iki.fi>
2015-02-24 17:00:19 +0200

    If no dev-target element, disable dev-tools

--- src/cljs/om_dev_tools/core.cljs
+++ src/cljs/om_dev_tools/core.cljs
@@ -13,17 +13,23 @@
 
 (defn root
   "Calls om/root but also initiates om-dev-tools.
+   If dev-target is nil, the dev tools is not used,
+   this way you can easily disable dev tools for production
+   by leaving the target element out of html.
 
    Options map takes two additional options:
    :dev-target    - (required) a DOM element.
-   :dev-state     - (required) a atom containing dev tools state."
+   :dev-state     - (required) a atom containing dev tools state.
+   :panels        - (optional  a map defining additional dev UI panels."
   [f value {:keys [dev-target dev-state panels] :as options}]
-  {:pre [dev-target dev-state]}
-  (let [inst-methods (instrumentation/instrumentation-methods dev-state)]
-    (om/root f value (-> options
-                         (dissoc :dev-target :dev-state :panels)
-                         (assoc :instrument (fn [f cursor m]
-                                              (om/build* f cursor (assoc m :descriptor inst-methods))))))
-    (om/root ui/dev-tools dev-state {:target dev-target
-                                     :opts {:app-state value
-                                            :panels panels}})))
+  {:pre [dev-state]}
+  (if dev-target
+    (let [inst-methods (instrumentation/instrumentation-methods dev-state)]
+      (om/root f value (-> options
+                           (dissoc :dev-target :dev-state :panels)
+                           (assoc :instrument (fn [f cursor m]
+                                                (om/build* f cursor (assoc m :descriptor inst-methods))))))
+      (om/root ui/dev-tools dev-state {:target dev-target
+                                       :opts {:app-state value
+                                              :panels panels}}))
+    (om/root f value (dissoc options :dev-target :dev-state :panels))))
